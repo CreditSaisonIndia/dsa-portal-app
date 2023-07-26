47d4c30e0989738db7499f2d75c4e29b338f24c4
shashank-k-y <shashank.sky.8@gmail.com>
2023-07-27 02:35:43 +0530

    create microfrontend app dsa-portal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Account } from './contexts/Account'
+import { CustomAxios } from './contexts/CustomAxios';
+import { ReactNotifications } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Amplify} from 'aws-amplify';
+import Home from './Home';
+import ProtectedRoute from './ProtectedRoute';
+import ForbiddenPage from './ForbiddenPage';
+import Dashboard from './Dashboard';
+
+
+Amplify.configure({
+    aws_cognito_region: 'us-east-1',
+    aws_user_pools_id: 'us-east-1_qY0eJKuWO',
+    aws_user_pools_web_client_id: 'ulab4268c8852f8bgsfbf5jgl',
+  });
+
+
+const App = () => {
+
+  return(
+  <BrowserRouter>
+  <Account>
+    <React.Fragment>
+    <ReactNotifications />
+      <Routes>
+      <Route path='dsaPortal' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+        <Route
+          path="dsaPortal/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard/>
+            </ProtectedRoute>
+          }
+        />
+        <Route path="dsaPortal/forbidden" element={<ForbiddenPage/>}
+        />
+      </Routes>
+    </React.Fragment>
+    </Account>
+  </BrowserRouter>
+  )
 }
 
 export default App;
