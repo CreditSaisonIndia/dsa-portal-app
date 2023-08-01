08b1b545963945328f926789141fd5f132236b4c
shashank-k-y <shashank.sky.8@gmail.com>
2023-08-01 13:55:20 +0530

    change navigate to signin

--- src/ForbiddenPage.jsx
+++ src/ForbiddenPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function ForbiddenPage() {
-
+  const navigate = useNavigate()
     console.log("Hi everyone");
   return (
     <div style={{textAlign: 'center'}}>
       <h2>Access Restricted</h2>
-      <a href='http://localhost:3000'><button>Sign In</button></a>
-
+      {/* <a href='http://localhost:3000'><button>Sign In</button></a> */}
+      <button onClick={() =>navigate('/')}>Sign In</button>
     </div>
   )
 }
